62c432b562bb5ae31ed922c81aa34a9f23e7d64c
satsukies <satsukies@gmail.com>
2015-08-30 23:01:33 +0900

    Adding DrawerMenu in RecommendationActivity

--- AWABMW/app/src/main/java/bmw/awa/awabmw/RecommendationActivity.java
+++ AWABMW/app/src/main/java/bmw/awa/awabmw/RecommendationActivity.java
@@ -1,5 +1,7 @@
 package bmw.awa.awabmw;
 
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,7 +9,14 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,7 +25,12 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.balysv.materialmenu.MaterialMenuDrawable;
+import com.balysv.materialmenu.extras.toolbar.MaterialMenuIconToolbar;
 import com.loopj.android.image.SmartImageView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -32,9 +46,23 @@ public class RecommendationActivity extends AppCompatActivity implements ViewPag
     SmartImageView imgJacket;
     @Bind(R.id.view_pager)
     ViewPager viewPager;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    SearchView mSearchView;
+    MaterialMenuIconToolbar menuIcon;
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mDrawerToggle;
+    RecyclerView mRecyclerView;
+    LinearLayoutManager mLayoutManager;
+    DrawerAdapter mAdapter;
+    boolean isDrawerOpened;
+
 
     String[] factor = new String[]{"album", "artist", "tempo", "tempo2", "tempo3"};
     int[] iconId = {R.drawable.album, R.drawable.artist, R.drawable.tempo, R.drawable.tempo, R.drawable.tempo};
+
+    String playingArtist, playingTrack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +70,134 @@ public class RecommendationActivity extends AppCompatActivity implements ViewPag
         setContentView(R.layout.activity_recommendation);
 
         ButterKnife.bind(this);
+
+        toolbar.inflateMenu(R.menu.menu_recommendation);
+        toolbar.setTitle("");
+
+        setSupportActionBar(toolbar);
+
+        /**
+         * aaaaaaaa
+         */
+
+        // ドロワーの開け閉めをActionBarDrawerToggleで監視
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.app_name, R.string.app_name);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerLayout.setDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                menuIcon.setTransformationOffset(
+                        MaterialMenuDrawable.AnimationState.BURGER_ARROW,
+                        isDrawerOpened ? 2 - slideOffset : slideOffset
+                );
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+//                super.onDrawerOpened(drawerView);
+                isDrawerOpened = true;
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+//                super.onDrawerClosed(drawerView);
+                isDrawerOpened = false;
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+//                super.onDrawerStateChanged(newState);
+                if(newState == DrawerLayout.STATE_IDLE) {
+                    if(isDrawerOpened) menuIcon.setState(MaterialMenuDrawable.IconState.ARROW);
+                    else menuIcon.setState(MaterialMenuDrawable.IconState.BURGER);
+                }
+            }
+        });
+
+        // ドロワー開くボタン(三本線)を表示
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.drawer_view);
+
+        // RecyclerView内のItemサイズが固定の場合に設定すると、パフォーマンス最適化
+        mRecyclerView.setHasFixedSize(true);
+
+        // レイアウトの選択
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // XML
+        TypedArray drawerMenuList = getResources().obtainTypedArray(R.array.drawer_menu_list);
+        int menuLength = drawerMenuList.length();
+
+        // RecyclerView.Adapter に渡すデータ
+        ArrayList<HashMap<String, Object>> drawerMenuArr = new ArrayList<>();
+
+        for (int i = 0; i < menuLength; i++) {
+            TypedArray itemArr = getResources().obtainTypedArray(drawerMenuList.getResourceId(i, 0));
+            int itemLength = itemArr.length();
+            HashMap<String, Object> content = new HashMap<>();
+            drawerMenuArr.add(content);
+            for (int j = 0; j < itemLength; j++) {
+                TypedArray contentArr = getResources().obtainTypedArray(itemArr.getResourceId(j, 0));
+
+                // key-value
+                if (contentArr.getString(0).contains("icon")) {
+                    content.put(contentArr.getString(0), contentArr.getDrawable(1));
+                } else {
+                    content.put(contentArr.getString(0), contentArr.getString(1));
+                }
+                contentArr.recycle();
+            }
+            itemArr.recycle();
+        }
+
+        // XMLを読込んで表示する
+//        String[] drawerMenuArr = getResources().getStringArray(R.array.drawer_list_arr);
+        mAdapter = new DrawerAdapter(drawerMenuArr);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        /**
+         * aaaaaaaaa
+         */
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuIcon.animateState(MaterialMenuDrawable.IconState.ARROW);
+            }
+        });
+
+        menuIcon = new MaterialMenuIconToolbar(this, Color.WHITE, MaterialMenuDrawable.Stroke.THIN) {
+            @Override
+            public int getToolbarViewId() {
+                return R.id.toolbar;
+            }
+        };
+
+        mSearchView = (SearchView) toolbar.getMenu().findItem(R.id.menu_search).getActionView();
+        mSearchView.setIconified(false);
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+//        get Intent and parse Extras-----
+//        Intent mIntent = getIntent();
+//        playingArtist = mIntent.getStringExtra("artist");
+//        playingTrack = mIntent.getStringExtra("track");
+
 
         imgJacket.setImageDrawable(getResources().getDrawable(R.drawable.jacket));
         imgJacket.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
@@ -127,9 +283,34 @@ public class RecommendationActivity extends AppCompatActivity implements ViewPag
     }
 
     @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        isDrawerOpened = mDrawerLayout.isDrawerOpen(Gravity.LEFT); // or END, LEFT, RIGHT
+        menuIcon.syncState(savedInstanceState);
+
+        // ActionBarDrawerToggleとMainActivityの状態を同期する
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // ActionBarDrawerToggleにイベント渡す
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        menuIcon.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_recommendation, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
         return true;
     }
 
@@ -140,8 +321,14 @@ public class RecommendationActivity extends AppCompatActivity implements ViewPag
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        switch (item.getItemId()) {
+            case R.menu.menu_recommendation:
+
+        }
+
+        // ActionBarDrawerToggleにイベント渡す
+        // 渡さないと、ドロワーボタンを押しても開かない
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
